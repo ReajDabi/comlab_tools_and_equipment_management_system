7043ea9753192878956210f1aae86ac61a7409d9
ReajDabi <r.dabi.147242.tc@umindanao.edu.ph>
2026-09-11 11:27:43 +0800

    Add project files.

---
+++ comlab_tools_and_equipment_management_system/Form1.cs
@@ -0,0 +1,10 @@
+namespace comlab_tools_and_equipment_management_system
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
