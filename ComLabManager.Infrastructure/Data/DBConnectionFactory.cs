8cfbf2e47d30e110e502c48a56b7183b62e04c04
ReajDabi <r.dabi.147242.tc@umindanao.edu.ph>
2026-09-13 17:51:36 +0800

    feat: added a DatabaseConnector

---
+++ ComLabManager.Infrastructure/Data/DBConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace ComlabManager.Infrastructure.Data
+{
+    public class DbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public DbConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        
+        public IDbConnection CreateConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+    }
+}
