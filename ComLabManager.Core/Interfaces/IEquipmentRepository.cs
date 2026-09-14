35e729491ab175a1d4002c9f1873000d2d399fff
ReajDabi <r.dabi.147242.tc@umindanao.edu.ph>
2026-09-14 17:47:50 +0800

    fix:  Inject dependencies

--- ComLabManager.Core/Interfaces/IEquipmentRepository.cs
+++ ComLabManager.Core/Interfaces/IEquipmentRepository.cs
@@ -6,10 +6,7 @@ namespace ComLabManager.Core.Interfaces
 {
     public interface IEquipmentRepository
     {
-        IEnumerable<Equipment> GetAll();
-        Equipment GetByAssetTag(string assetTag);
-        void Add(Equipment equipment);
-        void Update(Equipment equipment);
-        void Delete(int id);
+       List<Equipment> GetAllEquipment();
+        
     }
 }
