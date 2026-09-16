e362fadebab706b7535751b1a8b366ddf8d05c7a
ReajDabi <r.dabi.147242.tc@umindanao.edu.ph>
2026-09-16 14:03:29 +0800

    feat: added User authenticator

---
+++ ComLabManager.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using BCrypt.Net;
+using ComlabManager.Core.Interfaces;
+using ComLabManager.Core.Models;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace ComlabManager.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly string _connectionString;
+
+        public UserRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public User AuthenticateUser(string username, string rawPassword)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                string sql = "SELECT * FROM Users WHERE Username = @Username LIMIT 1";
+
+                
+                var user = connection.QuerySingleOrDefault<User>(sql, new { Username = username });
+
+                if (user != null && BCrypt.Net.BCrypt.Verify(rawPassword, user.PasswordHash))
+                {
+                    return user; 
+                }
+
+                return null; 
+            }
+        }
+    }
+}
