1953f368b8919484cfbd8c67120e04afd31e0d26
ReajDabi <r.dabi.147242.tc@umindanao.edu.ph>
2026-09-18 18:56:14 +0800

    feat: fix the pasword hashing for the admin

--- comlab_tools_and_equipment_management_system/Program.cs
+++ comlab_tools_and_equipment_management_system/Program.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 using System;
 using System.Windows.Forms;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+    
 
 namespace ComLabManager.UI
 {
@@ -19,23 +23,44 @@ namespace ComLabManager.UI
             // 1. Define your MySQL Connection String
             string connectionString = "Server=localhost;Port=3306;Database=LabManagerDb;Uid=root;Pwd=102425;";
 
+
+
             // 2. Setup the Dependency Injection Container
             var services = new ServiceCollection();
+
+
 
             // 3. Register your tools (Whenever the UI asks for IEquipmentRepository, give it this one)
             services.AddTransient<IEquipmentRepository>(provider => new EquipmentRepository(connectionString));
             services.AddTransient<IUserRepository>(provider => new UserRepository(connectionString));
 
+
+
             // Register your main form
             services.AddTransient<MainForm>();
+
+
+
+
             services.AddTransient<LoginForm>();
+
+
 
 
             // 4. Build the factory and run the app
             var serviceProvider = services.BuildServiceProvider();
-            var loginForm = serviceProvider.GetRequiredService<LoginForm>();
+            
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                string realHash = BCrypt.Net.BCrypt.HashPassword("admin123");
+                string updateSql = "UPDATE Users SET PasswordHash = @Hash WHERE Username = 'admin_reaj'";
+                connection.Execute(updateSql, new { Hash = realHash });
+            }
+            // ------------------------------
 
+            var loginForm = serviceProvider.GetRequiredService<LoginForm>();
             if (loginForm.ShowDialog() == DialogResult.OK)
+      
             {
                 // If login was successful, run the main application
                 Application.Run(serviceProvider.GetRequiredService<MainForm>());
