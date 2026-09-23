01ebfaca847c05c86e296eb18198557542267085
ReajDabi <r.dabi.147242.tc@umindanao.edu.ph>
2026-09-23 18:29:01 +0800

    feat: added  App.config and modified the Program.cs

--- comlab_tools_and_equipment_management_system/Program.cs
+++ comlab_tools_and_equipment_management_system/Program.cs
@@ -3,7 +3,7 @@ using ComlabManager.Core.Interfaces;
 using ComlabManager.Infrastructure.Repositories;
 using ComLabManager.Core.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
-
+using System.Configuration;
 using System;
 using System.Windows.Forms;
 using Dapper;
@@ -20,23 +20,22 @@ namespace ComLabManager.UI
         {
             ApplicationConfiguration.Initialize();
 
-            // 1. Define your MySQL Connection String
-            string connectionString = "Server=localhost;Port=3306;Database=LabManagerDb;Uid=root;Pwd=102425;";
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
 
 
-            // 2. Setup the Dependency Injection Container
+           
             var services = new ServiceCollection();
 
 
 
-            // 3. Register your tools (Whenever the UI asks for IEquipmentRepository, give it this one)
+           
             services.AddTransient<IEquipmentRepository>(provider => new EquipmentRepository(connectionString));
             services.AddTransient<IUserRepository>(provider => new UserRepository(connectionString));
 
 
 
-            // Register your main form
+            
             services.AddTransient<MainForm>();
 
 
@@ -47,7 +46,6 @@ namespace ComLabManager.UI
 
 
 
-            // 4. Build the factory and run the app
             var serviceProvider = services.BuildServiceProvider();
             
             using (var connection = new MySqlConnection(connectionString))
@@ -56,18 +54,18 @@ namespace ComLabManager.UI
                 string updateSql = "UPDATE Users SET PasswordHash = @Hash WHERE Username = 'admin_reaj'";
                 connection.Execute(updateSql, new { Hash = realHash });
             }
-            // ------------------------------
+          
 
             var loginForm = serviceProvider.GetRequiredService<LoginForm>();
             if (loginForm.ShowDialog() == DialogResult.OK)
       
             {
-                // If login was successful, run the main application
+             
                 Application.Run(serviceProvider.GetRequiredService<MainForm>());
             }
             else
             {
-                // If they click the "X" on the login screen, exit completely
+               
                 Application.Exit();
             }
         }
