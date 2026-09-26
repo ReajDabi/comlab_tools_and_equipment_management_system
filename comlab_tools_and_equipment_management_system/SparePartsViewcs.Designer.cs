5213623a936c1cd35ca9913409802565008a8258
androidsheesh <degaomchrishian@gmail.com>
2026-09-26 22:13:14 +0800

    feat: Add UI spare parts management

---
+++ comlab_tools_and_equipment_management_system/SparePartsViewcs.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace ComLabManager.UI
+{
+    partial class SparePartsViewcs
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // SparePartsViewcs
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(248, 249, 250);
+            Name = "SparePartsViewcs";
+            Size = new Size(958, 564);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
